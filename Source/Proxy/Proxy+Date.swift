418cc6929d2812e91479ef4b8404b98afd86eb24
christine betts <chrstn@uw.edu>
2023-04-06 02:04:45 -0400

    Update Proxy+Date.swift

--- Source/Proxy/Proxy+Date.swift
+++ Source/Proxy/Proxy+Date.swift
@@ -6,27 +6,27 @@
 //
 
 import Foundation
-
-extension MutableProxy where Wrapped == Date {
-    
-    /// Sets an updated Date into your model.
-    /// - Parameter newValue: The updated value for the Date.
-    public func set(_ newValue: Wrapped) {
-        set(newValue: .date(newValue))
-    }
-
-}
-
-extension MutableProxy where Wrapped == Date? {
-    
-    /// Sets an updated Date into your model.
-    /// - Parameter newValue: The updated value for the Date.
-    public func set(_ newValue: Wrapped) {
-        if let newValue = newValue {
-            set(newValue: .date(newValue))
-        } else {
-            set(newValue: .primitive(.null))
-        }
-    }
-
-}
+//
+//extension MutableProxy where Wrapped == Date {
+//    
+//    /// Sets an updated Date into your model.
+//    /// - Parameter newValue: The updated value for the Date.
+//    public func set(_ newValue: Wrapped) {
+//        set(newValue: .date(newValue))
+//    }
+//
+//}
+//
+//extension MutableProxy where Wrapped == Date? {
+//    
+//    /// Sets an updated Date into your model.
+//    /// - Parameter newValue: The updated value for the Date.
+//    public func set(_ newValue: Wrapped) {
+//        if let newValue = newValue {
+//            set(newValue: .date(newValue))
+//        } else {
+//            set(newValue: .primitive(.null))
+//        }
+//    }
+//
+//}
